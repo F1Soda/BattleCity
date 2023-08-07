e208841bf7b68b092cbf33e3615db0e70fe29e40
Tim <golik.tima@gmail.com>
2023-08-07 12:52:15 +0500

    Added AIComponent begining

--- Source/Game/AIComponent.cpp
+++ Source/Game/AIComponent.cpp
@@ -3,24 +3,37 @@
 
 AIComponent::EEnemyState AIComponent::m_eEnemyState;
 
+
+
+
+
 AIComponent::AIComponent(Tank* pParrentTank)
 	: m_pParentTank(pParrentTank)
+	, durationBetweenStepAI(1000)
 {
 
 }
 
 void AIComponent::update(const double delta)
 {
-	switch (m_eEnemyState)
-	{
-	case AIComponent::EEnemyState::Ñhaotic:
+	static double currentFrameTime = 0;
 
-		break;
-	case AIComponent::EEnemyState::PursuePlayers:
-		break;
-	case AIComponent::EEnemyState::PursueEagle:
-		break;
+	if (currentFrameTime <= durationBetweenButtonsClicking)
+		currentFrameTime += delta;
+	else
+	{
+		switch (m_eEnemyState)
+		{
+		case AIComponent::EEnemyState::Ñhaotic:
+
+			break;
+		case AIComponent::EEnemyState::PursuePlayers:
+			break;
+		case AIComponent::EEnemyState::PursueEagle:
+			break;
+		}
 	}
+	
 
 	m_pParentTank->fire();
 }
--- Source/Game/AIComponent.h
+++ Source/Game/AIComponent.h
@@ -5,10 +5,23 @@ class Tank;
 class AIComponent
 {
 public:
+
+	enum class EEnemyState
+	{
+		Ñhaotic,
+		PursuePlayers,
+		PursueEagle
+	};
+
 	AIComponent(Tank* pParrentTank);
 
 	void update(const double delta);
 
+	static EEnemyState m_eEnemyState;
 private:
+
+
 	Tank* m_pParentTank;
+	double durationBetweenStepAI;
+	
 };
